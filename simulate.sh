1ad5e9d1bbf83f9b2d8415f96c4158ca9496e574
nelumbus-cloud <sb2ek@mtmail.mtsu.edu>
2026-02-01 20:47:01 -0600

    give source file #2

--- simulate.sh
+++ simulate.sh
@@ -8,9 +8,10 @@
 
 #SBATCH -o logs/%x_%j.out
 #SBATCH -e logs/%x_%j.err
+set -Eeuo pipefail
 
-
-source activate rain_sim
+source /opt/ohpc/pub/apps/miniconda/etc/profile.d/conda.sh
+conda activate rain_sim
 
 DATAROOT=/home/sb2ek/uhome/datasets/nuscenes_mini
 DEPTHROOT=/projects/sb2ek/datasets/nuscenes_depth_mini
